a2772ed9954a50a41fd9ca2526ba997633d4f88b
AngkurMondal <angkur002@gmail.com>
2024-09-07 18:05:37 +0600

    fixed bugs

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -13,9 +13,9 @@ export const TodoList = ({ tasks, updatedTask, toggleTask, deleteTask }) => {
     return tasks.filter((task) => {
       return (
         !task.deleted &&
-        ((filter == "active" && !task.done) ||
-          (filter == "completed" && task.done) ||
-          filter == "all")
+        ((filter === "active" && !task.done) ||
+          (filter === "completed" && task.done) ||
+          filter === "all")
       );
     });
   };
