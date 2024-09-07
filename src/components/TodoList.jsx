c352aaab458803d7552172bf8829b4b41295290f
AngkurMondal <angkur002@gmail.com>
2024-09-07 17:47:03 +0600

    filtered the tasks before showing

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -16,31 +16,33 @@ export const TodoList = ({ tasks, updatedTask, toggleTask, deleteTask }) => {
   const handleShowCompletedTasks = () => {
     setFilter("completed");
   };
+
+  const getFilteredTasks = () => {
+    return tasks.filter((task) => {
+      return (
+        (filter == "active" && !task.deleted) ||
+        (filter == "deleted" && task.deleted) ||
+        (filter == "completed" && task.done) ||
+        filter == "all"
+      );
+    });
+  };
+
   return (
     <Paper square={true} sx={{ marginTop: 2 }}>
       {tasks.length > 0 && (
         <List>
-          {tasks.map((task) => {
-            const { id, description, deleted, done } = task;
-            if (
-              (filter == "active" && !deleted) ||
-              (filter == "deleted" && deleted) ||
-              (filter == "completed" && done) ||
-              filter == "all"
-            ) {
-              return (
-                <TodoListItem
-                  key={id}
-                  id={id}
-                  description={description}
-                  checked={done}
-                  toggleTask={toggleTask}
-                  deleteTask={deleteTask}
-                  updatedTask={updatedTask}
-                />
-              );
-            }
-          })}
+          {getFilteredTasks().map(({ id, description, deleted, done }) => (
+            <TodoListItem
+              key={id}
+              id={id}
+              description={description}
+              checked={done}
+              toggleTask={toggleTask}
+              deleteTask={deleteTask}
+              updatedTask={updatedTask}
+            />
+          ))}
         </List>
       )}
       <Box sx={{ padding: 1 }}>
